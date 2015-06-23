aa5da139ae1f1403008c5ed52cfa07de0ecee7cc
Joeri Verdeyen <joeriv@yappa.be>
2015-06-23 13:58:29 +0200

    Added some console commands for installation

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-chmod -R 777 /var/www/app
+mkdir  /var/www/app/web/js
+chmod -R 0777 /var/www/app
+php /var/www/app/app/console assets:install --env=prod
 php /var/www/app/app/console doctrine:schema:create --env=prod
-php /var/www/app/app/console doctrine:fixtures:load --env=prod
-php /var/www/app/app/console bazinga:js-translation:dump
+php /var/www/app/app/console doctrine:fixtures:load -n --env=prod
+php /var/www/app/app/console bazinga:js-translation:dump --env=prod
+php /var/www/app/app/console fos:js-routing:dump --env=prod
 php /var/www/app/app/console assetic:dump --env=prod
 php /var/www/app/app/console cache:warmup --env=prod
 
